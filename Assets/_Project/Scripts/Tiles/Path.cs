d000dde17c64a950c2a9c485cbcf176795afda1b
Алексей де Винча <cyanv98@gmail.com>
2025-08-06 18:04:24 +0300

    created tiles and tilemap

---
+++ Assets/_Project/Scripts/Tiles/Path.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Tiles
+{
+    [CreateAssetMenu(fileName = "New Path", menuName = "Cookieman/Tiles/Path")]
+    public class Path : RuleTile<Path.Neighbor>
+    {
+        [SerializeField] private List<TileBase> wallTiles;
+        
+        public class Neighbor : RuleTile.TilingRule.Neighbor
+        {
+            public const int Wall = 4;
+        }
+
+        public override bool RuleMatch(int neighbor, TileBase tile)
+        {
+            switch (neighbor)
+            {
+                case Neighbor.Wall: return wallTiles.Contains(tile);
+            }
+            
+            return base.RuleMatch(neighbor, tile);
+        }
+    }
+}
